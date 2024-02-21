b68f2d52f49b13969e614bba1a73a9efad6dfc62
unknown <hazelrtech@gmail.com>
2024-02-21 16:34:27 -0500

    fix

--- train_tracker.lua
+++ train_tracker.lua
@@ -40,7 +40,7 @@ function track_train(message)
         if not local_train_tracking[station_name][train_name] then
             local_train_tracking[station_name][train_name] = {}
             local_train_tracking[station_name][train_name]["round_trip_time"] = 0
-            local_train_tracking[station_name][train_name]["last_update_at"] = now
+            local_train_tracking[station_name][train_name]["last_update_at"] = departed_at
         end
     end
 end
