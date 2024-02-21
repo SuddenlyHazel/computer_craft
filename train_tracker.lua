e0b705859c20e7c6d527211b8b2b6a435397dea6
unknown <hazelrtech@gmail.com>
2024-02-21 15:10:56 -0500

    better logging

--- train_tracker.lua
+++ train_tracker.lua
@@ -3,5 +3,6 @@ local modem = peripheral.find("modem", rednet.open)
 while true do
     local event, message = rednet.receive("base_station")
     print(event)
-    print(message)
+    print(message["name"])
+    print(message["depart_at"])
 end
