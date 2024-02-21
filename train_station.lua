e2d70b014c9e0039b2afd08f91a0a176aa621f1d
unknown <hazelrtech@gmail.com>
2024-02-21 14:26:06 -0500

    rednet

--- train_station.lua
+++ train_station.lua
@@ -3,6 +3,11 @@ local modem = peripheral.find("modem", rednet.open)
 rednet.host("transit_station","base_station")
 
 while true do
-    os.pullEvent("redstone")
+    local event = os.pullEvent("redstone")
     print("A redstone input has changed!")
+    if event == "left" then
+      rednet.broadcast("train_1", "base_station")
+    elseif event == "right" then
+      rednet.broadcast("train_2", "base_station")
+    end
   end
