6d27db88e230d7592b27598a6efccd31fe1f01dc
unknown <hazelrtech@gmail.com>
2024-02-21 16:27:07 -0500

    fix

--- train_tracker.lua
+++ train_tracker.lua
@@ -26,13 +26,12 @@ function track_train(message)
     if local_train_tracking[station_name] and local_train_tracking[station_name][train_name] then
         local last_time = local_train_tracking[station_name][train_name]["round_trip_time"]
 
-        local_train_tracking[station_name][train_name]["last_update_at"] = now
-
         if now - local_train_tracking[station_name][train_name]["last_update_at"] < 5 then
             print("debounce")
             return
         end -- debounce
 
+        local_train_tracking[station_name][train_name]["last_update_at"] = now
         local_train_tracking[station_name][train_name]["round_trip_time"] = (last_time + departed_at) / 2
     else
         print("first time seeing train")
