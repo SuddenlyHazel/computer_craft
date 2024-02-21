d974e271366f48c47c5d4265833ced7533f77367
unknown <hazelrtech@gmail.com>
2024-02-21 15:00:09 -0500

    step

--- train_station.lua
+++ train_station.lua
@@ -1,13 +1,13 @@
 local modem = peripheral.find("modem", rednet.open)
 
-rednet.host("transit_station","base_station")
-
 while true do
     local event = os.pullEvent("redstone")
     print("A redstone input has changed!")
-    if event == "left" then
+    if redstone.getInput("left") then
       rednet.broadcast("train_1", "base_station")
-    elseif event == "right" then
+      print("train_1")
+    elseif redstone.getInput("right") then
       rednet.broadcast("train_2", "base_station")
+      print("train_2")
     end
   end
--- train_tracker.lua
+++ train_tracker.lua
@@ -1,6 +1,7 @@
 local modem = peripheral.find("modem", rednet.open)
 
 while true do
-    local event = rednet.receive("base_station")
+    local event, message = rednet.receive("base_station")
     print(event)
+    print(message)
 end
