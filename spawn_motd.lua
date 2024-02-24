63d6e3fc175c64be2742dfb424c07cf73dfd8747
Hazel <hazel@departure.dev>
2024-02-24 17:29:34 -0500

    test

--- spawn_motd.lua
+++ spawn_motd.lua
@@ -1,3 +1,10 @@
 local monitor = peripheral.find("monitor")
 monitor.setCursorPos(1, 1)
-monitor.write("Hello, world!")
+local resp = http.get("https://github.com/SuddenlyHazel/computer_craft/blob/main/motd.json")
+
+local body = resp.readAll();
+body = textutils.unserialiseJSON(body);
+
+for key, value in pairs(myTable) do
+  print(key, value)
+end
