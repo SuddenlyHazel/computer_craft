1e5f7ea1ddb7530a1d6a03e94fa48b136703f0c5
Hazel <hazel@departure.dev>
2024-02-24 17:40:13 -0500

    write to monitor

--- spawn_motd.lua
+++ spawn_motd.lua
@@ -1,11 +1,12 @@
 local monitor = peripheral.find("monitor")
 monitor.setCursorPos(1, 1)
 local resp = http.get("https://raw.githubusercontent.com/SuddenlyHazel/computer_craft/main/motd.json")
-print(resp)
 local body = resp.readAll();
-print(body)
 body = textutils.unserialiseJSON(body);
 
+local n = 1
 for key, value in pairs(body) do
-  print(key, value)
+  monitor.setCursorPos(1, n)
+  montior.write(value)
+  n = n + 1
 end
