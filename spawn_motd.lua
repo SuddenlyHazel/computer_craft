9a45e1773206da5ff95257c1e1a3801395ac7152
Hazel <hazel@departure.dev>
2024-02-24 17:45:16 -0500

    debug and error

--- spawn_motd.lua
+++ spawn_motd.lua
@@ -1,12 +1,17 @@
-local monitor = peripheral.find("monitor")
-monitor.setCursorPos(1, 1)
+attached_monitor = peripheral.find("monitor")
+
+-- Check if the monitor was found
+if attached_monitor == nil then
+  error("Monitor not found. Please ensure a monitor is connected.")
+end
+
 local resp = http.get("https://raw.githubusercontent.com/SuddenlyHazel/computer_craft/main/motd.json")
 local body = resp.readAll();
 body = textutils.unserialiseJSON(body);
 
 local n = 1
 for key, value in pairs(body) do
-  monitor.setCursorPos(1, n)
-  montior.write(value)
+  attached_monitor.setCursorPos(1,n)
+  attached_monitor.write(value)
   n = n + 1
 end
