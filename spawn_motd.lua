a871462b967c80592fb7d1e118b4ea204ea87ace
Hazel <hazel@departure.dev>
2024-02-24 17:56:21 -0500

    automatically update motd

--- spawn_motd.lua
+++ spawn_motd.lua
@@ -1,17 +1,24 @@
 attached_monitor = peripheral.find("monitor")
-
 -- Check if the monitor was found
 if attached_monitor == nil then
-  error("Monitor not found. Please ensure a monitor is connected.")
+    error("Monitor not found. Please ensure a monitor is connected.")
 end
 
-local resp = http.get("https://raw.githubusercontent.com/SuddenlyHazel/computer_craft/main/motd.json")
-local body = resp.readAll();
-body = textutils.unserialiseJSON(body);
+while true do
+    local now = os.epoch("local") / 1000;
+    print("refreshing motd", now)
+    
+    local resp = http.get("https://raw.githubusercontent.com/SuddenlyHazel/computer_craft/main/motd.json")
+    local body = resp.readAll();
+    body = textutils.unserialiseJSON(body);
 
-local n = 1
-for key, value in pairs(body) do
-  attached_monitor.setCursorPos(1,n)
-  attached_monitor.write(value)
-  n = n + 1
+    attached_monitor.clear()
+    local n = 1
+    for key, value in pairs(body) do
+        attached_monitor.setCursorPos(1, n)
+        attached_monitor.write(value)
+        n = n + 1
+    end
+    -- sleep for 30 seconds 
+    os.sleep(30)
 end
