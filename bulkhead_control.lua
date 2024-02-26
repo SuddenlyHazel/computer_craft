45b1a5ed630d0d9d91d3f5211ce644ffd3a1bd56
unknown <hazelrtech@gmail.com>
2024-02-25 21:16:25 -0500

    debug?

--- bulkhead_control.lua
+++ bulkhead_control.lua
@@ -5,9 +5,10 @@ bottom_sensor = colors.orange
 
 while true do
     os.pullEvent("redstone")
+    print("got event")
     local is_at_top = rs.testBundledInput("right", top_sensor)
-    local is_at_bottom = rs.testBundledInput("right", top_sensor)
-
+    local is_at_bottom = rs.testBundledInput("right", bottom_sensor)
+    print(is_at_top, is_at_bottom)
     if is_at_top then
         rs.setBundledOutput("right", clutch)
     elseif is_at_bottom then
