406d4a2ea36f6db5a364aa8e7bb6c4a0bcbec6b9
unknown <hazelrtech@gmail.com>
2024-02-26 12:42:36 -0500

    test

--- startup.lua
+++ startup.lua
@@ -34,35 +34,41 @@ function readConfig()
     return defaultOrExisting
 end
 
-function getRepoFile(hash, filename)
-end
-
-function readBootConfig(hash, config)
-    local resp = http.get(string.format(config["repo_url"],
-        hash, config["boot_config"]), {
+function getRepoFile(hash, repoUrl, filename)
+    local resp = http.get(string.format(repoUrl,
+        hash, filename), {
         ["Cache-Control"] = "no-store"
     })
 
     local body = resp.readAll();
     resp.close()
-    return textutils.unserialiseJSON(body);
+    return body
+end
+
+function readBootConfig(hash, config)
+    return textutils.unserialiseJSON(getRepoFile(hash, config["repo_url"], config["boot_config"]));
 end
 
 local config = readConfig()
 local currentHash = getHash(config["commit_url"])
 local lastHash = config["last_commit_hash"]
 
-function updateFiles(bootJson)
+function updateFiles(hash, bootJson, config)
     local programs = bootJson["programs"]
     for _, value in pairs(programs) do
-        pretty.pretty_print(value)
+        print(string.format("Fetching program | %s", value["id"]))
+        for _, filename in pairs(value["files"]) do
+            print(string.format("       fetching file %s", filename))
+            local progFile = getRepoFile(hash, config["repo_url"], filename)
+        end
     end
 end
+
 if lastHash ~= currentHash then
     print("Repo has been updated")
     print(string.format("last_sha: %s, current_sha: %s", lastHash, currentHash))
     print("Fetching boot.json..")
 
     local bootJson = readBootConfig(currentHash, config)
-    updateFiles(bootJson)
+    updateFiles(currentHash, bootJson, config)
 end
