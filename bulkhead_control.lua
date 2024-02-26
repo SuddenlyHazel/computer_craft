5671ce0294d15f2eae419d89911ea9cd67d2631b
unknown <hazelrtech@gmail.com>
2024-02-25 21:43:36 -0500

    async

--- bulkhead_control.lua
+++ bulkhead_control.lua
@@ -1,28 +1,31 @@
+peripheral.find("modem", rednet.open)
+
 clutch = colors.lightBlue
 gear_shift = colors.magenta
 top_sensor = colors.pink
 bottom_sensor = colors.orange
 
-::start::
+function drive_bulkhead()
+       os.pullEvent("redstone")
+
+        local is_at_top = rs.testBundledInput("right", top_sensor)
+        local is_at_bottom = rs.testBundledInput("right", bottom_sensor)
+
+        print(is_at_top, is_at_bottom)
+
+        if is_at_top then
+            local output = colors.combine(clutch)
+            rs.setBundledOutput("right", clutch)
+        elseif is_at_bottom then
+            rs.setBundledOutput("right", clutch)
+        end
+end
+
+function listen_for_command()
+    local id, message = rednet.receive("bulkhead_0001")
+    print(id, message)
+end
 
 while true do
-    local event, sender, message, protocol = os.pullEvent("redstone", "rednet_message")
-
-    if protocol ~= nill then
-        print("got rednet message")
-        goto start
-    end 
-
-    print("got event")
-    local is_at_top = rs.testBundledInput("right", top_sensor)
-    local is_at_bottom = rs.testBundledInput("right", bottom_sensor)
-
-    print(is_at_top, is_at_bottom)
-
-    if is_at_top then
-        local output = colors.combine(clutch)
-        rs.setBundledOutput("right", clutch)
-    elseif is_at_bottom then
-        rs.setBundledOutput("right", clutch)
-    end
+    parallel.waitForAny(drive_bulkhead, listen_for_command)
 end
