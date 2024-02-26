fba4ff2abe6ad048966b21ba0d8e2e515858d9fa
unknown <hazelrtech@gmail.com>
2024-02-26 10:13:07 -0500

    fix

--- factory_gui.lua
+++ factory_gui.lua
@@ -6,21 +6,15 @@ if attached_monitor == nil then
 end
 
 local monitor_size_x, monitor_size_y = attached_monitor.getSize()
-local comp = term.redirect(attached_monitor)
-
-paintutil.drawFilledBox(1, 1, monitor_size_x/4, monitor_size_y/4, colors.red)
-
-term.restore(comp)
-
 
 function drawButton(monitor, x, y, color, button_text)
     local old_m = term.redirect(monitor)
     monitor.setCursorPos(x + 2, y + 2)
     monitor.write(button_text)
     local text_end_x, text_end_y = monitor.getCursorPos()
-    paintutil.drawFilledBox(x, y, text_end_x + 2, y + 4, color)
+    paintutils.drawFilledBox(x, y, text_end_x + 2, y + 4, color)
     term.restore(old_m)
-end 
+end
 
 
 drawButton(attached_monitor, 1, 1, colors.blue, "hello world")
