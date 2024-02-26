782bcbb749fbc4f21ea60e3589b46b348040087d
unknown <hazelrtech@gmail.com>
2024-02-26 10:22:44 -0500

    test

--- factory_gui.lua
+++ factory_gui.lua
@@ -8,13 +8,16 @@ if attached_monitor == nil then
 end
 
 local monitor_size_x, monitor_size_y = attached_monitor.getSize()
+local grid_count_x = 10
+local grid_count_y = 15
+
+local grid_size_x = monitor_size_x / grid_count_x
+local grid_size_y = monitor_size_x / grid_count_y
+
 print(monitor_size_x, monitor_size_y)
 function drawButton(monitor, x, y, color, button_text)
-    local old_m = term.redirect(monitor)
-    monitor.setCursorPos(x + 2, y + 2)
-    monitor.write(button_text)
-    local text_end_x, text_end_y = monitor.getCursorPos()
-    paintutils.drawBox(x, y, x+10, y + 5, color)
+    term.redirect(monitor)
+    paintutils.drawBox(x, y, x+grid_size_x*3, y + grid_count_y*2, color)
     term.redirect(term.native())
 end
 
