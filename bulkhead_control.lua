a71b43de0c964e79535383a75a03db4372e29290
unknown <hazelrtech@gmail.com>
2024-02-25 21:04:29 -0500

    test for bulkhead

---
+++ bulkhead_control.lua
@@ -0,0 +1,8 @@
+clutch = colors.lightBlue
+gear_shift = colors.magenta
+top_sensor = colors.pink
+bottom_sensor = colors.orange
+
+while true do
+    print(rs.testBundledInput("right", top_sensor))
+end
