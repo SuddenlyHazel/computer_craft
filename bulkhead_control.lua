f42a2eded3471462d7033b5ebf87e0c6143eee5d
unknown <hazelrtech@gmail.com>
2024-02-25 22:35:40 -0500

    edit input

--- bulkhead_control.lua
+++ bulkhead_control.lua
@@ -1,5 +1,7 @@
 peripheral.find("modem", rednet.open)
 
+cable_input = "back"
+
 clutch = colors.lightBlue
 gear_shift = colors.magenta
 top_sensor = colors.pink
@@ -9,28 +11,28 @@ function drive_bulkhead()
         print("drive bulkhead_0001")
        os.pullEvent("redstone")
 
-        local is_at_top = rs.testBundledInput("right", top_sensor)
-        local is_at_bottom = rs.testBundledInput("right", bottom_sensor)
-        local is_going_up = rs.testBundledInput("right", gear_shift)
+        local is_at_top = rs.testBundledInput(cable_input, top_sensor)
+        local is_at_bottom = rs.testBundledInput(cable_input, bottom_sensor)
+        local is_going_up = rs.testBundledInput(cable_input, gear_shift)
 
         print(is_at_top, is_at_bottom)
 
         if is_at_top and is_going_up then
             local output = colors.combine(colors.subtract(clutch, gear_shift))
-            rs.setBundledOutput("right", clutch)
+            rs.setBundledOutput(cable_input, clutch)
         elseif is_at_bottom and not is_going_up then
-            rs.setBundledOutput("right", colors.combine(clutch, gear_shift))
+            rs.setBundledOutput(cable_input, colors.combine(clutch, gear_shift))
         end
 end
 
 function listen_for_command()
     print("drive bulkhead_0001")
-    local current_output = rs.getBundledOutput("right")
+    local current_output = rs.getBundledOutput(cable_input)
 
-    local is_at_top = rs.testBundledInput("right", top_sensor)
+    local is_at_top = rs.testBundledInput(cable_input, top_sensor)
 
     local id, message = rednet.receive("bulkhead_0001")
-    rs.setBundledOutput("right", colors.subtract(current_output, clutch))
+    rs.setBundledOutput(cable_input, colors.subtract(current_output, clutch))
 end
 
 while true do
