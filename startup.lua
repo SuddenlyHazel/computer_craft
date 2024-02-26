e8953d786f3f5842fbe663cf904e94739508ac8e
unknown <hazelrtech@gmail.com>
2024-02-26 12:04:58 -0500

    test

--- startup.lua
+++ startup.lua
@@ -3,6 +3,7 @@ SETTINGS_KEY = "hazel.computer_craft"
 
 DEFAULT_SETTING = {
     ["commit_url"] = "https://api.github.com/repos/SuddenlyHazel/computer_craft/commits/main",
+    ["repo_url"] = "https://raw.githubusercontent.com/SuddenlyHazel/computer_craft/%s/%s",
     ["last_commit_hash"] = "",
     ["boot_config"] = "boot.json",
     ["boot_program"] = nil,
@@ -20,6 +21,9 @@ function readConfig()
     return settings.get(SETTINGS_KEY, DEFAULT_SETTING)
 end
 
+function getRepoFile(hash, filename)
+end
+
 local config = readConfig()
 local currentHash = getHash(config["commit_url"])
 local lastHash = config["last_commit_hash"]
@@ -27,5 +31,15 @@ local lastHash = config["last_commit_hash"]
 if lastHash ~= currentHash then
     print("Repo has been updated")
     print(string.format("last_sha: %s, current_sha: %s", lastHash, currentHash))
+    print("Fetching boot.json..")
+
+    local resp = http.get(string.format(config["repo_url"],
+        currentHash, config["boot_config"]), {
+        ["Cache-Control"] = "no-store"
+    })
+
+    local body = resp.readAll();
+    print(body)
+    resp.close()
 end
 
