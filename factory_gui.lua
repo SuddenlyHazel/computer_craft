e0d425fc5494b2fc97304444b9c5c8c8ca768780
unknown <hazelrtech@gmail.com>
2024-02-27 11:53:45 -0500

    fix

--- factory_gui.lua
+++ factory_gui.lua
@@ -41,5 +41,4 @@ while true do
     local event, side, x, y = os.pullEvent("monitor_touch")
     print("The monitor on side " .. side .. " was touched at (" .. x .. ", " .. y .. ")")
     print(buttonOne.hitTest(x, y))
-    print(buttonTwo.hitTest(x, y))
 end
