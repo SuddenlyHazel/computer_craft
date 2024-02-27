1a4462f84b0e559e31660950143a60da531b92ab
unknown <hazelrtech@gmail.com>
2024-02-27 16:29:01 -0500

    try alarm code

--- bulkhead_control.lua
+++ bulkhead_control.lua
@@ -1,4 +1,6 @@
 peripheral.find("modem", rednet.open)
+speaker = peripheral.find("speaker")
+ALARM = "mekanism:tile.machine.industrial_alarm"
 
 cable_input = "back"
 
@@ -9,16 +11,34 @@ bottomSensor = colors.orange
 
 currentState = nil
 
+IS_RUNNING = false
+
+function runAlarm()
+    os.pullEvent("bulkhead_started")
+    while true do
+        local event = os.pullEvent("bulkhead_stopped", "speaker_audio_empty")
+        if event == "speaker_audio_empty" then
+            speaker.playSound(ALARM)
+        else
+            return runAlarm()
+        end
+    end
+end
+
 function drive_bulkhead()
     os.pullEvent("redstone")
+
+    local running = false;
 
     local is_at_top = rs.testBundledInput(cable_input, topSensor)
     local is_at_bottom = rs.testBundledInput(cable_input, bottomSensor)
     local is_going_up = rs.testBundledInput(cable_input, gearShift)
 
     if is_at_top and is_going_up then
+        os.queueEvent("bulkhead_stopped")
         rs.setBundledOutput(cable_input, clutch)
     elseif is_at_bottom and not is_going_up then
+        os.queueEvent("bulkhead_stopped")
         rs.setBundledOutput(cable_input, colors.combine(clutch, gearShift))
     end
 end
@@ -28,6 +48,8 @@ function listen_for_command()
     local current_output = rs.getBundledOutput(cable_input)
 
     local id, message = rednet.receive("bulkhead_0001")
+    os.queueEvent("bulkhead_started")
+    sleep(1)
     rs.setBundledOutput(cable_input, colors.subtract(current_output, clutch))
 end
 
@@ -35,5 +57,5 @@ while true do
     if currentState == nil then
         -- we don't know where the door is
     end
-    parallel.waitForAny(drive_bulkhead, listen_for_command)
+    parallel.waitForAny(drive_bulkhead, listen_for_command, runAlarm)
 end
