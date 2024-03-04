bb44512bbba0fd8e4e5fe87ef5dce8cc7046ecde
unknown <hazelrtech@gmail.com>
2024-03-04 15:10:35 -0500

    update

--- drone.lua
+++ drone.lua
@@ -1,11 +1,30 @@
 ---@class Drone
 Drone = {}
-Drone.__index = Drone -- Set the __index metamethod to the class table itself
-Drone._mtIndex = {}
+
 Drone.methodMetadata = {}
 
+Drone.__index = function(table, key)
+    local value = rawget(Drone, key) -- Attempt to get the method directly from the class
+    local metadata = Drone.methodMetadata[key]
+    -- Check if the method exists and is tagged
+    print(key)
+    if type(value) == "function" and metadata and metadata._dronePrecheck then
+        return function(self, ...)
+            if self:isConnected() then
+                return value(self, ...)
+            else
+                print("Drone is not connected!")
+                return nil
+            end
+        end
+    else
+        -- Return the original value (could be a method or nil)
+        return value
+    end
+end
+
 function buildFromInterface(...)
-    local args = {...}
+    local args = { ... }
     local output = {}
     for _, v in pairs(args) do
         table.insert(output, Drone:new(v["iName"], v))
@@ -14,7 +33,7 @@ function buildFromInterface(...)
 end
 
 function Drone:new(name, droneInterface)
-    local self = setmetatable({}, Drone._mtIndex) -- Create a new table and set its metatable to the class
+    local self = setmetatable({}, Drone) -- Create a new table and set its metatable to the class
     self.droneInterface = droneInterface
     self.isShowingArea = false
     self.name = name
@@ -28,12 +47,13 @@ function Drone:gotoLocation(p)
     self.droneInterface.setAction("goto")
 end
 
-Drone.methodMetadata["gotoLocation"] = {_dronePrecheck = true}
+Drone.methodMetadata["gotoLocation"] = { _dronePrecheck = true }
 
 function Drone:getPressure()
     self.droneInterface.getDronePressure()
 end
-Drone.methodMetadata["getPressure"] = {_dronePrecheck = true}
+
+Drone.methodMetadata["getPressure"] = { _dronePrecheck = true }
 
 function Drone:standby()
     print("drone entering standby")
@@ -42,7 +62,7 @@ function Drone:standby()
     self.droneInterface.setAction("standby")
 end
 
-Drone.methodMetadata["standby"] = {_dronePrecheck = true}
+Drone.methodMetadata["standby"] = { _dronePrecheck = true }
 
 function Drone:isConnected()
     self.droneInterface.isConnectedToDrone()
@@ -56,26 +76,6 @@ function Drone:toggleShowArea()
     end
 end
 
-Drone.methodMetadata["toggleShowArea"] = {_dronePrecheck = true}
+Drone.methodMetadata["toggleShowArea"] = { _dronePrecheck = true }
 
-Drone._mtIndex.__index = function(table, key)
-        local value = rawget(Drone, key) -- Attempt to get the method directly from the class
-        local metadata = Drone.methodMetadata[key]
-        -- Check if the method exists and is tagged
-        print(key)
-        if type(value) == "function" and metadata and metadata._dronePrecheck then
-            return function(self, ...)
-                if self:isConnected() then
-                    return value(self, ...)
-                else
-                    print("Drone is not connected!")
-                    return nil
-                end
-            end
-        else
-            -- Return the original value (could be a method or nil)
-            return value
-        end
-    end
-
-return {Drone = Drone, buildFromInterface = buildFromInterface}
+return { Drone = Drone, buildFromInterface = buildFromInterface }
