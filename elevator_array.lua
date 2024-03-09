4044e75656b6fd125abfe80f0cf993897ab7e7b5
unknown <hazelrtech@gmail.com>
2024-03-09 11:40:24 -0500

    update code

--- elevator_array.lua
+++ elevator_array.lua
@@ -1,11 +1,44 @@
+---@class RedstoneIntegrator
+---@field getInput fun(side: string):boolean
+---@field setOutput fun(side: string, powered: boolean)
+---@field getOutput fun(side: string):boolean
+---@type RedstoneIntegrator[]
+---@diagnostic disable-next-line
 INTEGRATORS = { peripheral.find("redstoneIntegrator") }
 
 print(("Found N=%s Integrators"):format(#INTEGRATORS))
 
-function redstoneListener()
-    local event, output = os.pullEvent("redstone")
+---comment
+---@param integrator RedstoneIntegrator
+local function integratorWatcher(integrator)
+    ---@diagnostic disable-next-line
+    print(("Starting Integrator Listener Loop for %s"):format(peripheral.getName(integrator)))
+    local lastState = integrator.getInput("bottom")
+    local lastStateAt = os.epoch("utc")
+
+    while true do
+        sleep(0.5)
+        local now = os.epoch("utc")
+        local currentState = integrator.getInput("bottom")
+        local currentOutput = integrator.getOutput("front")
+
+        if currentState ~= lastState then
+            lastState = currentState
+            lastStateAt = now
+        end
+
+        if now + 2 > lastStateAt and currentState ~= currentOutput then
+            integrator.setOutput("front", currentState)
+        end
+    end
 end
 
-while true do
-    print(redstoneListener())
+local hdls = {}
+
+for _, integrator in pairs(INTEGRATORS) do
+    table.insert(hdls, function()
+        integratorWatcher(integrator)
+    end)
 end
+
+parallel.waitForAll(table.unpack(hdls))
